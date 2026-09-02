f54b978b0c19d5a2a0a8c9c0851d1729687f77ac
Strwbrym0cha <Byoungharris@gmail.com>
2026-09-01 21:41:29 -0500

    Bring original V4 money and boss rooms into V5

---
+++ v17/tabs/v4-room.js
@@ -0,0 +1,8 @@
+const room=(root,view,title)=>{
+ root.innerHTML=`<iframe class="v5-v4-room" title="${title}" src="../v4/?view=${view}&embed=1"></iframe>`;
+};
+
+function renderMoneyV4({root}){room(root,'money','Money Café')}
+function renderWorkV4({root}){room(root,'boss','Boss Bitch')}
+
+export{renderMoneyV4,renderWorkV4};
