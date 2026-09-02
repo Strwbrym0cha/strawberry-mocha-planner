1909107d8eac3456811ba7a0d93ad7d91dce7cd2
Strwbrym0cha <Byoungharris@gmail.com>
2026-09-02 08:46:33 -0500

    Finalize V4 to V5 data migration

--- v5/app.js
+++ v5/app.js
@@ -1,4 +1,4 @@
-import{loadV5Ui,saveV5Ui,saveV5DailyNote,saveV5RoomDetail,saveV5LedgerEntry,removeV5LedgerEntry,snapshotV4,migrateV4ToV5}from'./data.js?v=5.0.3-v4-migration';
+import{loadV5Ui,saveV5Ui,saveV5DailyNote,saveV5RoomDetail,saveV5LedgerEntry,removeV5LedgerEntry,snapshotV4,migrateV4ToV5}from'./data.js?v=5.0.4-v4-migration';
 import{renderBoss}from'./boss.js?v=5.0.0-preview.2';
 import{renderRoom}from'./rooms.js?v=5.0.0-official';
 
