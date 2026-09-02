5294d643846a48f0714b47cd8469d2cfaa570430
Strwbrym0cha <Byoungharris@gmail.com>
2026-09-01 21:54:07 -0500

    Apply V5 pink lavender matcha skin to restored rooms

--- v17/tabs/v4-room.js
+++ v17/tabs/v4-room.js
@@ -1,5 +1,5 @@
 const room=(root,view,title)=>{
- root.innerHTML=`<iframe class="v5-v4-room" title="${title}" src="../v4/?view=${view}&embed=1"></iframe>`;
+ root.innerHTML=`<iframe class="v5-v4-room" title="${title}" src="../v4/?view=${view}&embed=1&v=4.1.26"></iframe>`;
 };
 
 function renderMoneyV4({root}){room(root,'money','Money Café')}
