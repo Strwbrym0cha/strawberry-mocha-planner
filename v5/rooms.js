aa5c308ebe5f6f7037d0a58142763e0bf5bfaeb3
Strwbrym0cha <Byoungharris@gmail.com>
2026-09-02 08:30:12 -0500

    Publish official KatOS V5 build

--- v5/rooms.js
+++ v5/rooms.js
@@ -1,4 +1,4 @@
-import{loadV5DailyNote,loadV5RoomDetail,ledgerSummary}from'./data.js?v=5.4.2-all-popups';
+import{loadV5DailyNote,loadV5RoomDetail,ledgerSummary}from'./data.js?v=5.0.0-official';
 
 const list=value=>Array.isArray(value)?value:[];
 const obj=value=>value&&typeof value==='object'&&!Array.isArray(value)?value:{};
